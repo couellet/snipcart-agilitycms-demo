74c8ce71a482921dbad86dc9a504d98d441900c1
Charles Ouellet <charles.ouellet@gmail.com>
2020-12-09 11:49:55 -0500

    Added getCustomInitialProps static method

--- components/agility-modules/ProductsList.js
+++ components/agility-modules/ProductsList.js
@@ -6,4 +6,25 @@ const ProductsList = (props) => {
     );
 };
 
+ProductsList.getCustomInitialProps = async ({
+    agility,
+    languageCode}) => {
+    const rawProducts = await agility.getContentList({
+        referenceName: 'products',
+        languageCode,
+    });
+
+    return {
+        products: rawProducts.map(x => {
+            return {
+                productId: x.fields.productId,
+                name: x.fields.name,
+                image: x.fields.image.url,
+                price: x.fields.price,
+                description: x.fields.description
+            }
+        })
+    };
+}
+
 export default ProductsList;
