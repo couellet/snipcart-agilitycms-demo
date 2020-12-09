e4e4490e736c540238f4cf79d585918bd8f1838d
Charles Ouellet <charles.ouellet@gmail.com>
2020-12-09 14:20:35 -0500

    Created products API

---
+++ pages/api/products.js
@@ -0,0 +1,39 @@
+import agility from "@agility/content-fetch";
+
+// setup content fetch
+const api = agility.getApi({
+  guid: process.env.AGILITY_GUID,
+  apiKey: process.env.AGILITY_API_FETCH_KEY,
+});
+
+// get products
+const getProducts = async () => {
+  const data = api
+    .getContentList({
+      referenceName: "products",
+      languageCode: "en-us",
+    })
+    .then(function (contentList) {
+      return contentList;
+    })
+    .catch(function (error) {
+      console.log("Error: " + error);
+    });
+  return data;
+};
+
+export default async (req, res) => {
+  const response = await getProducts();
+
+  const products = response.items.map((product) => {
+    return {
+      title: product.fields.name,
+      id: product.fields.productId,
+      image: product.fields.image.url,
+      price: product.fields.price,
+      description: product.fields.description,
+    };
+  });
+
+  return res.status(200).json(products);
+};
