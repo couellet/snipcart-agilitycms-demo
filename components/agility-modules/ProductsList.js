4cd7f060c50eeed25acb7299d1bc3080d22b0ba2
Charles Ouellet <charles.ouellet@gmail.com>
2020-12-10 12:06:18 -0500

    Initial state for real

--- components/agility-modules/ProductsList.js
+++ components/agility-modules/ProductsList.js
@@ -1,44 +1,9 @@
-import tw from 'twin.macro';
-import Product from './Product';
-
-const Container = tw.div`container mx-auto grid gap-5 grid-cols-1 sm:grid-cols-3 mb-8`;
-
-const Header = tw.h1`container mt-1 text-2xl font-bold`;
-
-const ProductsList = ({customData}) => {
-    const { products } = customData;
-    console.log(products);
+const ProductsList = (props) => {
     return (
         <div>
-            <Header>Products list</Header>
-            <Container>
-                {products.map((product, i) => (
-                    <Product product={product} key={i} />
-                ))}
-            </Container>
+            <h1>Products list</h1>
         </div>
-    );
-};
-
-ProductsList.getCustomInitialProps = async ({
-    agility,
-    languageCode}) => {
-    const rawProducts = await agility.getContentList({
-        referenceName: 'products',
-        languageCode,
-    });
-
-    return {
-        products: rawProducts.map(x => {
-            return {
-                productId: x.fields.productId,
-                name: x.fields.name,
-                image: x.fields.image.url,
-                price: x.fields.price,
-                description: x.fields.description
-            }
-        })
-    };
+    )
 }
 
 export default ProductsList;
